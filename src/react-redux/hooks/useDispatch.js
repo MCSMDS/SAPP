88d9b7a65666846e6ed60a566760412e25a68437
MCSMDS <39450832+MCSMDS@users.noreply.github.com>
2020-08-17 18:41:28 +0800

    updata

--- src/react-redux/hooks/useDispatch.js
+++ src/react-redux/hooks/useDispatch.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import ReactReduxContext from '../components/Context'
 import { useStore as useDefaultStore, createStoreHook } from './useStore'
 
-const ReactReduxContext = React.createContext(null);
 export function createDispatchHook(context = ReactReduxContext) {
   const useStore = context === ReactReduxContext ? useDefaultStore : createStoreHook(context)
   return () => useStore().dispatch

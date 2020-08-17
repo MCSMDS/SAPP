32029a68213749ada1d0f8b1d439d9094fc51653
MCSMDS <MCSMDS@users.noreply.github.com>
2020-08-17 10:38:11 +0000

    deploy: dd4598d703efb67b37f8afdcfca11892404f38c7

---
+++ precache-manifest.1253960ab8d13eea581e5af99699725e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96096a12bbd5475f1edbfdf86e5519d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61ee9167c3f080968517",
+    "url": "/static/js/2.8e82a888.chunk.js"
+  },
+  {
+    "revision": "d455b55662ec59e842e850740eeaf3d2",
+    "url": "/static/js/2.8e82a888.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66de55b893263a1af27e",
+    "url": "/static/js/main.7ae93a96.chunk.js"
+  },
+  {
+    "revision": "5fa058d22ce90b0453a0",
+    "url": "/static/js/runtime-main.cbe12837.js"
+  }
+]);

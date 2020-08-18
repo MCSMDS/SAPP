8f999ba9d88f85a0434a86571183db91dc00ee3c
MCSMDS <39450832+MCSMDS@users.noreply.github.com>
2020-08-18 17:52:43 +0800

    Update selectorFactory.js

--- src/react-redux/selectorFactory.js
+++ src/react-redux/selectorFactory.js
@@ -2,6 +2,7 @@ export default function pureFinalPropsSelectorFactory({ mapStateToProps, mapDisp
 
   function is(x, y) {
     if (x === y) {
+      console.log(x !== 0 || y !== 0 || 1 / x === 1 / y)
       return x !== 0 || y !== 0 || 1 / x === 1 / y;
     } else {
       return false;

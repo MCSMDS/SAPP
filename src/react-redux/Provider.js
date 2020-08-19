6efaab855197ce983617619903e834f82ca29cd5
MCSMDS <39450832+MCSMDS@users.noreply.github.com>
2020-08-19 17:02:35 +0800

    Update Provider.js

--- src/react-redux/Provider.js
+++ src/react-redux/Provider.js
@@ -5,14 +5,6 @@ import Subscription from './Subscription'
 export default function Provider({ store, children }) {
   const subscription = new Subscription(store)
   subscription.onStateChange = subscription.notifyNestedSubs
-  const contextValue = { store, subscription }
-  useEffect(() => {
-    contextValue.subscription.trySubscribe()
-    return () => {
-      contextValue.subscription.tryUnsubscribe()
-      contextValue.subscription.onStateChange = null
-      console.log(1111111111)
-    }
-  }, [store, contextValue])
+  subscription.trySubscribe()
   return <Context.Provider value={{ store, subscription }}>{children}</Context.Provider>
 }

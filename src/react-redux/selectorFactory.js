8f288dd228c9d2b39d801b06b649ed4fbf7d2b84
MCSMDS <39450832+MCSMDS@users.noreply.github.com>
2020-08-19 17:54:15 +0800

    Revert "Update selectorFactory.js"
    This reverts commit 39bed6a70869f16b90f0e47b0f85263698b60512.

--- src/react-redux/selectorFactory.js
+++ src/react-redux/selectorFactory.js
@@ -3,6 +3,19 @@ export default function pureFinalPropsSelectorFactory(dispatch) {
   const mapStateToProps = state => ({ getStorage: key => state[key] })
   const mapDispatchToProps = dispatch => ({ setStorage: (key, value) => dispatch({ type: key, value }) })
 
+  function shallowEqual(objA, objB) {
+    return true;
+    /*     if (objA === objB) return true;
+        if (typeof objA !== 'object' || objA === null || typeof objB !== 'object' || objB === null) return false;
+        const keysA = Object.keys(objA);
+        const keysB = Object.keys(objB);
+        if (keysA.length !== keysB.length) return false;
+        for (let i = 0; i < keysA.length; i++) {
+          if (!Object.prototype.hasOwnProperty.call(objB, keysA[i]) || !(objA[keysA[i]] === objB[keysA[i]])) return false;
+        }
+        return true; */
+  }
+
   let hasRunAtLeastOnce = false
   let state
   let ownProps
@@ -31,12 +44,23 @@ export default function pureFinalPropsSelectorFactory(dispatch) {
     return mergedProps
   }
 
+  function handleNewState() {
+    const nextStateProps = mapStateToProps(state)
+    const statePropsChanged = !shallowEqual(nextStateProps, stateProps)
+    stateProps = nextStateProps
+    if (statePropsChanged) mergedProps = ({ ...ownProps, ...stateProps, ...dispatchProps });
+    return mergedProps
+  }
+
   function handleSubsequentCalls(nextState, nextOwnProps) {
+    const propsChanged = !shallowEqual(nextOwnProps, ownProps)
     const stateChanged = nextState !== state
     state = nextState
     ownProps = nextOwnProps
-    if (stateChanged) return handleNewPropsAndNewState()
-    return handleNewProps()
+    if (propsChanged && stateChanged) return handleNewPropsAndNewState()
+    if (propsChanged) return handleNewProps()
+    if (stateChanged) return handleNewState()
+    return mergedProps
   }
 
   return function pureFinalPropsSelector(nextState, nextOwnProps) {

6c80e98a77894fa0aab0458c17fbb3d3326f2f4a
MCSMDS <39450832+MCSMDS@users.noreply.github.com>
2020-08-19 19:22:56 +0800

    Update connect.js

--- src/react-redux/connect.js
+++ src/react-redux/connect.js
@@ -1,70 +1,73 @@
-import hoistStatics from 'hoist-non-react-statics'
-import React, { useContext, useMemo, useRef, useReducer, useLayoutEffect } from 'react'
-import Subscription from './Subscription'
-import Context from './Context'
-import selectorFactory from './selectorFactory'
-
-const useIsomorphicLayoutEffectWithArg = (effectFunc, effectArgs, dependencies) => useLayoutEffect(() => effectFunc(...effectArgs), [effectFunc, effectArgs, dependencies])
+import hoistStatics from "hoist-non-react-statics";
+import React, { useContext, useMemo, useRef, useReducer, useLayoutEffect } from "react";
+import Subscription from "./Subscription";
+import Context from "./Context";
+import selectorFactory from "./selectorFactory";
 
 function captureWrapperProps(lastWrapperProps, lastChildProps, renderIsScheduled, wrapperProps, actualChildProps, childPropsFromStoreUpdate, notifyNestedSubs) {
-  lastWrapperProps.current = wrapperProps
-  lastChildProps.current = actualChildProps
-  renderIsScheduled.current = false
-  childPropsFromStoreUpdate.current = null
-  notifyNestedSubs()
+  lastWrapperProps.current = wrapperProps;
+  lastChildProps.current = actualChildProps;
+  renderIsScheduled.current = false;
+  childPropsFromStoreUpdate.current = null;
+  notifyNestedSubs();
 }
 
 function subscribeUpdates(store, subscription, childPropsSelector, lastWrapperProps, lastChildProps, renderIsScheduled, childPropsFromStoreUpdate, notifyNestedSubs, forceComponentUpdateDispatch) {
-  let didUnsubscribe = false
+  let didUnsubscribe = false;
   const checkForUpdates = () => {
-    if (didUnsubscribe) return
-    let newChildProps = childPropsSelector(store.getState(), lastWrapperProps.current)
+    if (didUnsubscribe) return;
+    let newChildProps = childPropsSelector(
+      store.getState(),
+      lastWrapperProps.current
+    );
     if (newChildProps === lastChildProps.current) {
-      if (!renderIsScheduled.current) notifyNestedSubs()
+      if (!renderIsScheduled.current) notifyNestedSubs();
     } else {
-      lastChildProps.current = newChildProps
-      childPropsFromStoreUpdate.current = newChildProps
-      renderIsScheduled.current = true
-      forceComponentUpdateDispatch({ type: 'STORE_UPDATED', payload: {} })
+      lastChildProps.current = newChildProps;
+      childPropsFromStoreUpdate.current = newChildProps;
+      renderIsScheduled.current = true;
+      forceComponentUpdateDispatch({ type: "STORE_UPDATED", payload: {} });
     }
-  }
-  subscription.onStateChange = checkForUpdates
-  subscription.trySubscribe()
-  checkForUpdates()
+  };
+  subscription.onStateChange = checkForUpdates;
+  subscription.trySubscribe();
+  checkForUpdates();
   return () => {
-    didUnsubscribe = true
-    subscription.tryUnsubscribe()
-    subscription.onStateChange = null
-  }
+    didUnsubscribe = true;
+    subscription.tryUnsubscribe();
+    subscription.onStateChange = null;
+  };
 }
 
-
-
 export default function wrapWithConnect(WrappedComponent) {
-  const usePureOnlyMemo = useMemo
+  const usePureOnlyMemo = useMemo;
 
   function ConnectFunction(props) {
-    const contextValue = useContext(Context)
-    const store = contextValue.store
-    const childPropsSelector = useMemo(() => selectorFactory(store.dispatch), [store])
-    const subscription = new Subscription(store, contextValue.subscription)
-    const notifyNestedSubs = subscription.notifyNestedSubs.bind(subscription)
-    const [previousStateUpdateResult, forceComponentUpdateDispatch] = useReducer((state, action) => action.payload, null)
-    const lastChildProps = useRef()
-    const lastWrapperProps = useRef(props)
-    const childPropsFromStoreUpdate = useRef()
-    const renderIsScheduled = useRef(false)
+    const contextValue = useContext(Context);
+    const store = contextValue.store;
+    const childPropsSelector = useMemo(() => selectorFactory(store.dispatch), [store]);
+    const subscription = new Subscription(store, contextValue.subscription);
+    const notifyNestedSubs = subscription.notifyNestedSubs.bind(subscription);
+    const [previousStateUpdateResult, forceComponentUpdateDispatch] = useReducer((state, action) => action.payload, null);
+    const lastChildProps = useRef();
+    const lastWrapperProps = useRef(props);
+    const childPropsFromStoreUpdate = useRef();
+    const renderIsScheduled = useRef(false);
 
     const actualChildProps = usePureOnlyMemo(() => {
       if (childPropsFromStoreUpdate.current && props === lastWrapperProps.current) return childPropsFromStoreUpdate.current;
-      return childPropsSelector(store.getState(), props)
-    }, [store, previousStateUpdateResult, props])
+      return childPropsSelector(store.getState(), props);
+    }, [store, previousStateUpdateResult, props]);
 
-    useIsomorphicLayoutEffectWithArg(captureWrapperProps, [lastWrapperProps, lastChildProps, renderIsScheduled, props, actualChildProps, childPropsFromStoreUpdate, notifyNestedSubs])
-    useIsomorphicLayoutEffectWithArg(subscribeUpdates, [store, subscription, childPropsSelector, lastWrapperProps, lastChildProps, renderIsScheduled, childPropsFromStoreUpdate, notifyNestedSubs, forceComponentUpdateDispatch], [store, subscription, childPropsSelector])
+    useLayoutEffect(() => captureWrapperProps(lastWrapperProps, lastChildProps, renderIsScheduled, props, actualChildProps, childPropsFromStoreUpdate, notifyNestedSubs));
+    useLayoutEffect(() => subscribeUpdates(store, subscription, childPropsSelector, lastWrapperProps, lastChildProps, renderIsScheduled, childPropsFromStoreUpdate, notifyNestedSubs, forceComponentUpdateDispatch), [store, subscription, childPropsSelector, notifyNestedSubs]);
 
-    return <Context.Provider value={{ ...contextValue, subscription }}><WrappedComponent {...actualChildProps} /></Context.Provider>
+    return (
+      <Context.Provider value={{ ...contextValue, subscription }}>
+        <WrappedComponent {...actualChildProps} />
+      </Context.Provider>
+    );
   }
 
-  return hoistStatics(React.memo(ConnectFunction), WrappedComponent)
+  return hoistStatics(React.memo(ConnectFunction), WrappedComponent);
 }

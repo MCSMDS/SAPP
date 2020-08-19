34d19b56fc23b04ab4797c3e11d3c1f22a12140d
MCSMDS <39450832+MCSMDS@users.noreply.github.com>
2020-08-19 17:44:26 +0800

    Update selectorFactory.js

--- src/react-redux/selectorFactory.js
+++ src/react-redux/selectorFactory.js
@@ -4,15 +4,16 @@ export default function pureFinalPropsSelectorFactory(dispatch) {
   const mapDispatchToProps = dispatch => ({ setStorage: (key, value) => dispatch({ type: key, value }) })
 
   function shallowEqual(objA, objB) {
-    if (objA === objB) return true;
-    if (typeof objA !== 'object' || objA === null || typeof objB !== 'object' || objB === null) return false;
-    const keysA = Object.keys(objA);
-    const keysB = Object.keys(objB);
-    if (keysA.length !== keysB.length) return false;
-    for (let i = 0; i < keysA.length; i++) {
-      if (!Object.prototype.hasOwnProperty.call(objB, keysA[i]) || !(objA[keysA[i]] === objB[keysA[i]])) return false;
-    }
     return true;
+    /*     if (objA === objB) return true;
+        if (typeof objA !== 'object' || objA === null || typeof objB !== 'object' || objB === null) return false;
+        const keysA = Object.keys(objA);
+        const keysB = Object.keys(objB);
+        if (keysA.length !== keysB.length) return false;
+        for (let i = 0; i < keysA.length; i++) {
+          if (!Object.prototype.hasOwnProperty.call(objB, keysA[i]) || !(objA[keysA[i]] === objB[keysA[i]])) return false;
+        }
+        return true; */
   }
 
   let hasRunAtLeastOnce = false

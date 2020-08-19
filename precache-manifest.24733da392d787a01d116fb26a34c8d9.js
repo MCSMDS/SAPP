0c682e99bdd1b3358ddf6cb6735cfc9360012f3d
MCSMDS <MCSMDS@users.noreply.github.com>
2020-08-19 06:56:41 +0000

    deploy: 461d10381034f0e24e6f990be63f9a82b605d14f

---
+++ precache-manifest.24733da392d787a01d116fb26a34c8d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71c0f35d898b888da72359c3969af8c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89c023052eb95ad5a429",
+    "url": "/static/js/2.51e8071a.chunk.js"
+  },
+  {
+    "revision": "d455b55662ec59e842e850740eeaf3d2",
+    "url": "/static/js/2.51e8071a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaf3bfba9e94bdbdcddf",
+    "url": "/static/js/main.8ca2a76c.chunk.js"
+  },
+  {
+    "revision": "5fa058d22ce90b0453a0",
+    "url": "/static/js/runtime-main.cbe12837.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aa07050c2ce024e798cf1c422e934fea.js"
+  "/precache-manifest.24733da392d787a01d116fb26a34c8d9.js"
 );
 
 self.addEventListener('message', (event) => {

12d128ac6af77d0f2b4e3b61683d2fa48c7076d2
MCSMDS <39450832+MCSMDS@users.noreply.github.com>
2020-08-19 19:35:25 +0800

    Update connect.js

--- src/react-redux/connect.js
+++ src/react-redux/connect.js
@@ -4,41 +4,6 @@ import Subscription from "./Subscription";
 import Context from "./Context";
 import selectorFactory from "./selectorFactory";
 
-function captureWrapperProps(lastWrapperProps, lastChildProps, renderIsScheduled, wrapperProps, actualChildProps, childPropsFromStoreUpdate, notifyNestedSubs) {
-  lastWrapperProps.current = wrapperProps;
-  lastChildProps.current = actualChildProps;
-  renderIsScheduled.current = false;
-  childPropsFromStoreUpdate.current = null;
-  notifyNestedSubs();
-}
-
-function subscribeUpdates(store, subscription, childPropsSelector, lastWrapperProps, lastChildProps, renderIsScheduled, childPropsFromStoreUpdate, notifyNestedSubs, forceComponentUpdateDispatch) {
-  let didUnsubscribe = false;
-  const checkForUpdates = () => {
-    if (didUnsubscribe) return;
-    let newChildProps = childPropsSelector(
-      store.getState(),
-      lastWrapperProps.current
-    );
-    if (newChildProps === lastChildProps.current) {
-      if (!renderIsScheduled.current) notifyNestedSubs();
-    } else {
-      lastChildProps.current = newChildProps;
-      childPropsFromStoreUpdate.current = newChildProps;
-      renderIsScheduled.current = true;
-      forceComponentUpdateDispatch({ type: "STORE_UPDATED", payload: {} });
-    }
-  };
-  subscription.onStateChange = checkForUpdates;
-  subscription.trySubscribe();
-  checkForUpdates();
-  return () => {
-    didUnsubscribe = true;
-    subscription.tryUnsubscribe();
-    subscription.onStateChange = null;
-  };
-}
-
 export default function wrapWithConnect(WrappedComponent) {
   const usePureOnlyMemo = useMemo;
 
@@ -59,14 +24,42 @@ export default function wrapWithConnect(WrappedComponent) {
       return childPropsSelector(store.getState(), props);
     }, [store, previousStateUpdateResult, props]);
 
-    useLayoutEffect(() => captureWrapperProps(lastWrapperProps, lastChildProps, renderIsScheduled, props, actualChildProps, childPropsFromStoreUpdate, notifyNestedSubs));
-    useLayoutEffect(() => subscribeUpdates(store, subscription, childPropsSelector, lastWrapperProps, lastChildProps, renderIsScheduled, childPropsFromStoreUpdate, notifyNestedSubs, forceComponentUpdateDispatch), [store, subscription, childPropsSelector, notifyNestedSubs]);
+    useLayoutEffect(() => {
+      lastWrapperProps.current = props;
+      lastChildProps.current = actualChildProps;
+      renderIsScheduled.current = false;
+      childPropsFromStoreUpdate.current = null;
+      notifyNestedSubs();
+    });
 
-    return (
-      <Context.Provider value={{ ...contextValue, subscription }}>
-        <WrappedComponent {...actualChildProps} />
-      </Context.Provider>
-    );
+    useLayoutEffect(() => {
+      let didUnsubscribe = false;
+      const checkForUpdates = () => {
+        if (didUnsubscribe) return;
+        let newChildProps = childPropsSelector(
+          store.getState(),
+          lastWrapperProps.current
+        );
+        if (newChildProps === lastChildProps.current) {
+          if (!renderIsScheduled.current) notifyNestedSubs();
+        } else {
+          lastChildProps.current = newChildProps;
+          childPropsFromStoreUpdate.current = newChildProps;
+          renderIsScheduled.current = true;
+          forceComponentUpdateDispatch({ type: "STORE_UPDATED", payload: {} });
+        }
+      };
+      subscription.onStateChange = checkForUpdates;
+      subscription.trySubscribe();
+      checkForUpdates();
+      return () => {
+        didUnsubscribe = true;
+        subscription.tryUnsubscribe();
+        subscription.onStateChange = null;
+      };
+    }, [store, subscription, childPropsSelector, notifyNestedSubs]);
+
+    return <Context.Provider value={{ ...contextValue, subscription }}><WrappedComponent {...actualChildProps} /></Context.Provider>;
   }
 
   return hoistStatics(React.memo(ConnectFunction), WrappedComponent);
